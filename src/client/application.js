8b7aee3bd6eba0754d7b61613037237877aa8f00
darwin <oleg.umarov@gmail.com>
2016-07-14 04:45:06 -0500

    clean slate

--- src/client/application.js
+++ src/client/application.js
@@ -1,12 +1,6 @@
-import $ from 'jquery';
-import {blegh} from 'shared/test';
 import './application.scss';
 
 if (module.hot) {
   module.hot.accept();
 }
 
-blegh();
-
-$('body').html('WHOA');
-console.log('YESSS');
