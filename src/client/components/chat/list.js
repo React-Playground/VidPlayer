a175ae9ecba31ab7151ac0e8f19cc07133dc976b
darwin <oleg.umarov@gmail.com>
2016-07-17 09:13:59 -0500

    setup chat

--- src/client/components/chat/list.js
+++ src/client/components/chat/list.js
@@ -1,4 +1,6 @@
 import $ from 'jquery';
+import moment from 'moment';
+import {Observable} from 'rxjs';
 import {ElementComponent} from '../../lib/component.js';
 
 export class ChatListComponent extends ElementComponent {
@@ -8,12 +10,24 @@ export class ChatListComponent extends ElementComponent {
     this._users = userStore;
     this._chat = chatStore;
     this.$element.addClass('chat-messages');
-    console.log(this);
   }
 
   _onAttach() {
-    this._chat.messages$.compSubscribe(this, message => {
-      this.$element.append($('<li />').text(message.message));
-    });
+    Observable.merge(
+      this._chat.messages$.map(chatMessageFactory))
+      .filter(m => m)
+      .compSubscribe(this, $newElement => {
+        this.$element.append($newElement);
+      });
   }
+}
+
+function chatMessageFactory({user, message, type, time}) {
+  return $(`<li class='message' ${type} />`)
+    .data('user', user.name)
+    .append([
+      $('<span class="author" />').text(user.name).css('color', user.color),
+      $('<span class="message" />').text(message),
+      $('<span class="time" />').text(moment(time).format('h:mm:ss a'))
+    ]);
 }
